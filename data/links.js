6f758ae96ec48843b5247f40be3aab7e0ed38d04
Alex Spencer <alexander.j.spencer@gmail.com>
2019-08-22 08:45:25 -0700

    building data object

---
+++ data/links.js
@@ -0,0 +1,123 @@
+module.exports = [
+    {
+        name: 'Augusta National',
+        location: 'Augusta, Georgia',
+        par: 72,
+        yards: 7435,
+        architect: 'Alister MacKenzie',
+        yearBuilt: 1933,
+        url: 'assets/augusta national.png',
+        hasHostedMajor: true, //Insert boolean value here
+    },
+    {
+        name: 'Pine Valley GC',
+        location: 'Pine Valley, New Jersey',
+        par: 70,
+        yards: 7057,
+        architect: 'Hugh Wilson',
+        yearBuilt: 1913,
+        url: 'assets/pine-valley.png',
+        hasHostedMajor: false, //Insert boolean value here
+    },
+    {
+        name: 'Cypress Point Club',
+        location: 'Pebble Beach, California',
+        par: 72,
+        yards: 6524,
+        architect: 'Alister MacKenzie',
+        yearBuilt: 1928,
+        url: 'assets/cypress-point.png',
+        hasHostedMajor: false, //Insert boolean value here
+    },
+    {
+        name: 'Shinnecock Hills GC',
+        location: 'Southampton, New York',
+        par: 70,
+        yards: 7041,
+        architect: 'C.B. Macdonald',
+        yearBuilt: 1891,
+        url: 'assets/shinnecock-hills.png',
+        hasHostedMajor: true, //Insert boolean value here
+    },
+    {
+        name: 'The Old Course at St. Andrews Links',
+        location: 'St. Andrews, Scotland',
+        par: 72,
+        yards: 7279,
+        architect: 'Old Tom Morris',
+        yearBuilt: 1552,
+        url: 'assets/old-course.png',
+        hasHostedMajor: true, //Insert boolean value here
+    },
+    {
+        name: 'Oakmont',
+        location: 'Oakmont, Pennsylvania',
+        par: 71,
+        yards: 7255,
+        architect: 'H.C. Fownes',
+        yearBuilt: 1903,
+        url: 'assets/oakmont.png',
+        hasHostedMajor: true, //Insert boolean value here
+    },
+    {
+        name: 'Pebble Beach G Links',
+        location: 'Pebble Beach, California',
+        par: 72,
+        yards: 6828,
+        architect: 'Jack Neville',
+        yearBuilt: 1919,
+        url: 'assets/pebble-beach.png',
+        hasHostedMajor: true, //Insert boolean value here
+    },
+    {
+        name: '',
+        location: '',
+        par: '',
+        yards: '',
+        architect: '',
+        yearBuilt: '',
+        url: '',
+        hasHostedMajor: '', //Insert boolean value here
+    },
+    {
+        name: '',
+        location: '',
+        par: '',
+        yards: '',
+        architect: '',
+        yearBuilt: '',
+        url: '',
+        hasHostedMajor: '', //Insert boolean value here
+    },
+    {
+        name: '',
+        location: '',
+        par: '',
+        yards: '',
+        architect: '',
+        yearBuilt: '',
+        url: '',
+        hasHostedMajor: '', //Insert boolean value here
+    },
+    {
+        name: '',
+        location: '',
+        par: '',
+        yards: '',
+        architect: '',
+        yearBuilt: '',
+        url: '',
+        hasHostedMajor: '', //Insert boolean value here
+    },
+    {
+        name: '',
+        location: '',
+        par: '',
+        yards: '',
+        architect: '',
+        yearBuilt: '',
+        url: '',
+        hasHostedMajor: '', //Insert boolean value here
+    },
+    
+]
---
+++ server.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+//Application dependencies
+
+
+//Database client
+
+
+//Application Setup
+
+
+//Start the server
+app.listen(PORT, () => {
+    console.log('server is running on PORT', PORT);
+});
