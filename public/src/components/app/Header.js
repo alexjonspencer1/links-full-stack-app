a167a5af9b46190ce9af22a6f6c2748359e71713
Alex Spencer <alexander.j.spencer@gmail.com>
2019-08-22 10:08:38 -0700

    component architecture applied to header

---
+++ public/src/components/app/Header.js
@@ -0,0 +1,21 @@
+import Component from '../Component.js';
+
+class Header extends Component {
+    renderHTML() {
+        const title = this.props.title || 'Famous Golf Courses';
+
+        return /*html*/`
+            <header>
+                <img class="header-logo" src="../public//assets//page logo.png" alt="golf-site-logo">
+                <h1>${title}</h1>
+                <nav>
+                    <a href="./">Home |</a>
+                    <a href="./links-list.html">Golf Courses |</a>
+                    <a href="./links-form.html">Add A Golf Course</a>
+                </nav>
+            </header>
+        `;
+    }
+}
+
+export default Header;
