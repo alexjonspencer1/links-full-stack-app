1ac23d49b8dff70fb3ddb96ba8f5fe263877f287
Alex Spencer <alexander.j.spencer@gmail.com>
2019-08-23 17:26:11 -0700

    kind of working

--- server.js
+++ server.js
@@ -23,16 +23,89 @@ app.use(express.json());
 app.get('/api/golfcourses', (req, res) => {
     client.query(`
         SELECT
-            name,
-            location,
-            region_id,
-            par,
-            yards,
-            architect,
-            year,
-            url,
-            hosted_a_major as "hasHostedMajor"
-        FROM GOLFCOURSE;
+            c.name,
+            c.location,
+            c.region_id,
+            t.region as region,
+            c.par,
+            c.yards,
+            c.architect,
+            c.year,
+            c.url,
+            c.hosted_a_major as "hasHostedMajor"
+        FROM golfcourse c
+        JOIN regions t
+        ON c.region_id = t.id
+        ORDER BY c.year
+    `)
+        .then(result => {
+            res.json(result.rows);
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err.message || err
+            });
+        });
+});
+
+app.get('/api/golfcourses/:id', (req, res) => {
+    const id = req.params.id;
+
+    client.query(`
+        SELECT 
+            c.*,
+            t.region as region
+        FROM golfcourse c
+        JOIN regions t
+        ON c.region_id = t.id
+        WHERE c.id = $1
+    `,
+    [id]
+    )
+        .then(result => {
+            const golfcourse = result.rows[0];
+            if(!golfcourse) {
+                res.status(404).json({
+                    error: `Golf course id ${id} does not exist`
+                });
+            }
+            else {
+                res.json(result.rows[0]);
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err.message || err
+            });
+        });
+});
+
+app.post('/api/golfcourses', (req, res) => {
+    const golfcourse = req.body;
+    client.query(`
+        INSERT INTO golfcourse (name, location, region_id, par, yards, architect, year, url, hosted_a_major)
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+        RETURNING *;
+    `,
+    [golfcourse.name, golfcourse.location, golfcourse.regionId, golfcourse.par, golfcourse.yards, golfcourse.architect, golfcourse.year, golfcourse.url, golfcourse.hasHostedMajor]
+    )
+        .then(result => {
+            res.json(result.rows[0]);
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err.message || err
+            });
+        });
+});
+
+app.get('/api/regions', (req, res) => {
+    client.query(`
+        SELECT
+            id,
+            region
+        FROM regions
+        ORDER BY region;
     `)
         .then(result => {
             res.json(result.rows);
